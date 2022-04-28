4fa8177a4cd1e6fcacf30e89a474f8f3e6bc00eb
vancomm <vanushnick@gmail.com>
2022-04-28 19:35:53 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { createServer } from 'http';
+
+const requestListener = function (req, res) {
+	res.writeHead(200);
+	res.end('Hello, World!');
+}
+
+const server = createServer(requestListener);
+server.listen(8080);
